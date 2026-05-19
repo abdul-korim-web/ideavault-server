4cd7db45ed8ff6dd2046ade0db69e535b7bc6067
abdul-korim-web <moneyschorsing@gmail.com>
2026-05-20 01:24:03 +0600

    versel remove

--- index.js
+++ index.js
@@ -36,8 +36,7 @@ app.use((err,req,res,next)=>{
 })
 
 
-// app.listen(5000,()=>{
-//     console.log(`server running on port 5000`);
-// })
+app.listen(5000,()=>{
+    console.log(`server running on port 5000`);
+})
 
-export default app;
