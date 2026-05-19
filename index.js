c32c861c6cdc211895433e50fe6772d2c9aa3a1f
abdul-korim-web <moneyschorsing@gmail.com>
2026-05-20 00:48:34 +0600

    versel setup

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ app.use((err,req,res,next)=>{
 })
 
 
-app.listen(5000,()=>{
-    console.log(`server running on port 5000`);
-})
+// app.listen(5000,()=>{
+//     console.log(`server running on port 5000`);
+// })
+
+export default app;
